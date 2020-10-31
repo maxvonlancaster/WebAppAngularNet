3f03eaa50b28ad5eb2e59666eacddd35c7cb37dc
windchange7 <windchange7@gmail.com>
2020-10-31 14:34:57 +0200

    controller initialization

---
+++ EWebApp/Controllers/PresentationsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EWebApp.DAL.Context;
+using EWebApp.DAL.Entities;
+
+namespace EWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PresentationsController : ControllerBase
+    {
+        private readonly PresentationContext _context;
+
+        public PresentationsController(PresentationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Presentations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Presentation>>> GetPresentations()
+        {
+            return await _context.Presentations.ToListAsync();
+        }
+
+        // GET: api/Presentations/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Presentation>> GetPresentation(long id)
+        {
+            var presentation = await _context.Presentations.FindAsync(id);
+
+            if (presentation == null)
+            {
+                return NotFound();
+            }
+
+            return presentation;
+        }
+
+        // PUT: api/Presentations/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPresentation(long id, Presentation presentation)
+        {
+            if (id != presentation.PresentationId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(presentation).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PresentationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Presentations
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Presentation>> PostPresentation(Presentation presentation)
+        {
+            _context.Presentations.Add(presentation);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPresentation", new { id = presentation.PresentationId }, presentation);
+        }
+
+        // DELETE: api/Presentations/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Presentation>> DeletePresentation(long id)
+        {
+            var presentation = await _context.Presentations.FindAsync(id);
+            if (presentation == null)
+            {
+                return NotFound();
+            }
+
+            _context.Presentations.Remove(presentation);
+            await _context.SaveChangesAsync();
+
+            return presentation;
+        }
+
+        private bool PresentationExists(long id)
+        {
+            return _context.Presentations.Any(e => e.PresentationId == id);
+        }
+    }
+}
