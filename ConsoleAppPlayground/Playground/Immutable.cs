88d7eee491266705e8a7c154349bc07b1afff8c1
Max <windchange7@gmail.com>
2021-01-01 23:43:34 +0200

    custom immutables

--- ConsoleAppPlayground/Playground/Immutable.cs
+++ ConsoleAppPlayground/Playground/Immutable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace ConsoleAppPlayground.Playground
@@ -12,8 +13,24 @@ namespace ConsoleAppPlayground.Playground
         }
     }
 
-    public class MyImmutableClass 
+    public class MyImmutableClass
     {
-    
+        private readonly ReadOnlyCollection<Item> myItems;
+
+        public ReadOnlyCollection<Item> Items 
+        {
+            get { return myItems; } 
+        }
+
+        public MyImmutableClass(List<Item> items)
+        {
+            myItems = items.AsReadOnly();
+        }
+    }
+
+    public class Item 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
