fd0f9fffb4ce0d832e2b3855a3ecceafe90fe70e
Max <windchange7@gmail.com>
2021-01-02 13:38:02 +0200

    Aspect oriented started

---
+++ ConsoleAppPlayground/Features/Aop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppPlayground.Features
+{
+    public class Aop
+    {
+        public void Main() 
+        {
+            var remoting = new RemotingObject();        
+            
+        }
+
+
+    }
+
+    public class RemotingObject : MarshalByRefObject 
+    {
+    
+    }
+}
