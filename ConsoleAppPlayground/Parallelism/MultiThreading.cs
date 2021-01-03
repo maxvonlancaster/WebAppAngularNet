563ec0dd106eb94a42572ed54c87ece77829beab
Max <windchange7@gmail.com>
2021-01-03 20:35:52 +0200

    locks

---
+++ ConsoleAppPlayground/Parallelism/MultiThreading.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppPlayground.Parallelism
+{
+    public class MultiThreading
+    {
+        public void Main() 
+        {
+            var locking = new Structure() { id = 1};
+            //lock (locking)  --> structure is not a reference type as required by locking 
+            //{
+            
+            //}
+        }
+    }
+
+    public struct Structure 
+    {
+        public int id;
+    }
+}
