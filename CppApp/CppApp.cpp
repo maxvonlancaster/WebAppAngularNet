515504b2f34ead5a7e1d6e5a38341a6be2e74105
Max <windchange7@gmail.com>
2021-03-13 00:49:50 +0200

    types cpp

--- CppApp/CppApp.cpp
+++ CppApp/CppApp.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <math.h>
+#include <typeinfo>
 
 
 int k;
@@ -13,6 +14,38 @@ void variables_and_types()
     double j = cos(i);
     std::cout << "cos of " << i << " : " << j << "\n";
     std::cout << "default value of unitialized int: " << k << "\n"; // 0
+
+    // Types:
+    bool b = true;
+    b = "a";
+    std::cout << "type of variable b: " << typeid(b).name() << "\n";
+    std::cout << "b: " << b << "\n"; // it prints 1, have no idea how it works, will dig into it later
+
+    char c = 'c';
+    signed char sc = 2;
+    unsigned char usc = 5; // symbol that has this code in ascii
+    wchar_t wct = '$'; // extended symbol, 2 bytes
+    char16_t csixtt = 'u'; // 2 bytes in unicode
+    char32_t cthirt = 'i'; // 4 bytes in unicode 
+    short sh = 32767; // 2 bytes in int
+    unsigned short ush = 65535; // 2 bytes from 0 
+    int integ = 2147483647; // 2 or 4 bytes depending on processor architecture 
+    unsigned int uinteg = 4294967295; // from 0
+    long l = 0; // 4 bytes
+    long long ll = 9223372036854775808; // 8 bytes 
+    float f = 4.435345; // floating point, 4 bytes 
+    double d = 0.1234234; // 8 bytes 
+    long double ld = 0.24356234897562987435629834756; // more then 8
+
+    std::wcout << usc << wct << '\n'; // this function for wchars
+    // standart puts only minimal limitations on the size of memory for each type
+    // depending on pc - bigger
+    // sizeof() -> gives size of memory:
+    long double n = 1;
+    std::cout << "sizeof(n)=" << sizeof(n) << "\n";
+
+    auto number = 5; // compiler desides type itself
+    //auto numb; -> compiler error - can not deduce type
 }
 
 int main()
