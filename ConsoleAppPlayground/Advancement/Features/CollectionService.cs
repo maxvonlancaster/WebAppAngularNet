4fbb06516e5f0d4f0a4a4737c5d150f58856e56c
Max <windchange7@gmail.com>
2021-03-18 00:01:14 +0200

    observable collection

--- ConsoleAppPlayground/Advancement/Features/CollectionService.cs
+++ ConsoleAppPlayground/Advancement/Features/CollectionService.cs
@@ -6,6 +6,8 @@ using System.Collections.Concurrent;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleAppPlayground.Advancement.Db.Entities;
+using System.Collections.ObjectModel;
 
 namespace ConsoleAppPlayground.Advancement.Features
 {
@@ -15,6 +17,7 @@ namespace ConsoleAppPlayground.Advancement.Features
         public void Main() 
         {
             ArrayListUsage();
+            ObservableCollectionUsage();
         }
 
         // most are from these namespaces:
@@ -64,27 +67,116 @@ namespace ConsoleAppPlayground.Advancement.Features
 
         public void LinkedListUsage() 
         {
-        
+            // LinkedList - every element has a link to previous elem and next 
+            // every elem is of type LinkedListNode<T>
+            // 
+            LinkedListNode<string> linkedListNode = new LinkedListNode<string>("a");
+            string val = linkedListNode.Value;
+            LinkedListNode<string> next = linkedListNode.Next;
+            LinkedListNode<string> prev = linkedListNode.Previous; // will be null, as they are not set
+
+            LinkedList<string> ll = new LinkedList<string>();
+            ll.AddLast("b"); // putting into the last place
+            ll.AddFirst("c"); //
+            ll.AddLast("x");
+            ll.AddAfter(ll.Last, "d");
+
+            LinkedListNode<string> node = ll.Find("c");
+            ll.AddBefore(node, "e");
+            ll.Remove(node);
+            ll.RemoveLast();
+            foreach (var elem in ll) 
+            {
+                Console.WriteLine(elem);
+            }
+            // + operation of insert is fast
+            // - search is slow 
         }
 
         public void QueueUsage() 
         {
-        
+            // Queue - first in first out;
+            Queue<double> q = new Queue<double>();
+            q.Enqueue(1.0); // adds to the end of queue
+            q.Enqueue(2.555);
+            q.Enqueue(3.14);
+            int count = q.Count; // 3
+            double first = q.Dequeue(); // first=1.0, q={2.555, 3.14}
+            double peeked = q.Peek(); // peeked=2.555, q={2.555, 3.14}, no deletion
         }
 
         public void StackUsage() 
         {
-        
+            // Stack - last in first out;
+            Stack<Product> s = new Stack<Product>();
+            s.Push(new Product() { Id = 1, Name = "1"});
+            s.Push(new Product() { Id = 2, Name = "2" });
+            s.Push(new Product() { Id = 3, Name = "3" });
+            Product last = s.Pop();
+            Product p = s.Peek(); // without deletion
         }
 
         public void DictionaryUsage() 
         {
-        
+            // every element is of type KeyValuePair<T, S>;
+            Dictionary<string, Product> dict = new Dictionary<string, Product>();
+            dict.Add("a", new Product() { Id = 1, Name = "a"});
+            dict.Add("b", new Product() { Id = 2, Name = "b" });
+            dict.Add("c", new Product() { Id = 3, Name = "c" });
+            dict.Add("d", new Product() { Id = 4, Name = "d" });
+
+            dict.Remove("b"); // removal by key
+            Product p = dict["c"]; // retrieval by key 
+            dict["c"] = new Product() { Id = 5, Name = "c" }; // adds new if not exists
+            foreach (string s in dict.Keys) { }
+            foreach (Product pr in dict.Values) { }
+
+            // starting from c# 6 new way of dict init:
+            Dictionary<int, int> dictPowers = new Dictionary<int, int>() 
+            {
+                [1]=0,
+                [2]=4,
+                [3]=9
+            };
+
         }
 
         public void ObservableCollectionUsage() 
         {
-        
+            // notifies outside objects about change in collection;
+            ObservableCollection<Product> products = new ObservableCollection<Product>() 
+            {
+                new Product(){ Id = 1, Name = "1"},
+                new Product(){ Id = 2, Name = "2"},
+                new Product(){ Id = 3, Name = "3"},
+                new Product(){ Id = 4, Name = "4"}
+            };
+
+            products.CollectionChanged += Products_CollectionChanged;
+
+            products.Add(new Product() { Id = 5, Name = "5"});
+            products.RemoveAt(1);
+            products[2] = new Product() { Id = 6, Name = "6"};
+        }
+
+        private void Products_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action) 
+            {
+                case NotifyCollectionChangedAction.Add: // in case of adding
+                    Product product = e.NewItems[0] as Product;
+                    Console.WriteLine("New prod added: " + product.Name);
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    Product productDel = e.OldItems[0] as Product;
+                    Console.WriteLine("Prod deleted: " + productDel.Name);
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                    Product p1 = e.OldItems[0] as Product;
+                    Product p2 = e.NewItems[0] as Product;
+                    Console.WriteLine($"Product {p1.Name} replaced with product {p2.Name}");
+                    break;
+            }
         }
 
         public void IenumerableIenumeratorUsage() 
