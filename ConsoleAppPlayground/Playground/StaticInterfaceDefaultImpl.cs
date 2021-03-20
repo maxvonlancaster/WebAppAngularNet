44918743563ffe06e21fa9c9fab4002660c39532
Max <windchange7@gmail.com>
2021-03-21 00:08:39 +0200

    default method in interface

---
+++ ConsoleAppPlayground/Playground/StaticInterfaceDefaultImpl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPlayground.Playground
+{
+    public class StaticInterfaceDefaultImpl
+    {
+        public void Main()
+        {
+            Trait trait = new Trait();
+            trait.GetNumber(1); // wow, default implementation of interface method :)
+        }
+    }
+
+    public interface ITrait { }
+    public class Trait : ITrait { }
+
+
+    public static class ExtensionMethods 
+    {
+        public static int GetNumber(this ITrait trait, int i) { return i + 1; }
+    }
+}
